b2c23fe047d75735a20bd786a79b07194a05f4b9
henintsoa98 <38418989+henintsoa98@users.noreply.github.com>
2024-04-18 22:58:17 +0300

    Update build.v1.0.bash

--- build.v1.0.bash
+++ build.v1.0.bash
@@ -1,13 +1,9 @@
-cd base
-docker build --no-cache --force-rm -t docker_open5gs .
-cd ../ims_base
-docker build --no-cache --force-rm -t docker_kamailio .
-cd ../srslte
-docker build --no-cache --force-rm -t docker_srslte .
-cd ../srsran
-docker build --no-cache --force-rm -t docker_srsran .
-cd ../ueransim
-docker build --no-cache --force-rm -t docker_ueransim .
+cd && cd docker_open5gs_160424/docker_open5gs
+cd base && docker build --no-cache --force-rm -t docker_open5gs .
+cd ../ims_base && docker build --no-cache --force-rm -t docker_kamailio .
+cd ../srslte && docker build --no-cache --force-rm -t docker_srslte .
+cd ../srsran && docker build --no-cache --force-rm -t docker_srsran .
+cd ../ueransim && docker build --no-cache --force-rm -t docker_ueransim .
 
 cd ..
 set -a
@@ -19,9 +15,7 @@ sudo cpupower frequency-set -g performance
 echo -ne "5g or 4g ? : "
 read REPLY
 if [[ "$REPLY" == "5g" || "$REPLY" == "5G" ]]; then
-	docker compose -f sa-deploy.yaml build
+	docker compose -f sa-deploy.yaml build && docker pull mongo:6.0
 else
-	docker compose -f sa-deploy.yaml build
+	docker compose -f sa-deploy.yaml build && docker pull mongo:6.0
 fi
-
-
